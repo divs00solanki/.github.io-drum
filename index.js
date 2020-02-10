eff8ae62a7add177a29fab7946cbf79206a3d079
divs00solanki <60890719+divs00solanki@users.noreply.github.com>
2020-02-10 22:01:37 +0530

    This a the first drum codes

---
+++ index.js
@@ -0,0 +1,80 @@
+
+for(var i = 0 ; i < document.querySelectorAll(".drum").length ; i++){
+document.querySelectorAll("button")[i].addEventListener("click", function()
+{
+
+  var buttonInnerHTML = this.innerHTML;
+  makeSound(buttonInnerHTML);
+  buttonAnimation(buttonInnerHTML);
+
+    // console.log(this);
+  //What to do
+  //Anonymous Function (No name of the function)
+
+  // keydown
+});
+}
+document.addEventListener("keydown", function(event){
+
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+        var audio = new Audio("crash.mp3");
+        audio.play();
+        break;
+    case "a":
+        var audio = new Audio("kick-bass.mp3");
+        audio.play();
+        break;
+    case "s":
+        var audio = new Audio("snare.mp3");
+        audio.play();
+        break;
+    case "d":
+        var audio = new Audio("tom-1.mp3");
+        audio.play();
+        break;
+
+    case "j":
+        var audio = new Audio("tom-2.mp3");
+        audio.play();
+        break;
+    case "k":
+        var audio = new Audio("tom-3.mp3");
+        audio.play();
+        break;
+    case "l":
+        var audio = new Audio("tom-4.mp3");
+        audio.play();
+        break;
+    default:console.log(buttonInnerHTML);
+
+  }
+
+
+
+
+}
+
+
+function buttonAnimation(currentKey){
+
+var activeButton =  document.querySelector("."+currentKey);
+
+activeButton.classList.add("pressed");
+
+setTimeout(function(){
+  activeButton.classList.remove("pressed");
+
+},100);
+
+}
+
+//
+// var audio = new Audio("sounds/crash.mp3");
+// audio.play();
